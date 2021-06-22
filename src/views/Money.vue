c5685364fb7cfe600355aa84dffd01b8b74cb1fa
Bryant-Wang24 <485969746@qq.com>
2021-06-22 17:54:39 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-      Money页面
+  <div class="nav-wrapper">
+    <div class="content">
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    <p>Money页面</p>
+    </div>
       <Nav/>
   </div>
 </template>
@@ -12,5 +53,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+      border: 1px solid green;
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+    }
+    .content{
+      border: 1px solid blue;
+      overflow: auto;
+      flex-grow: 1;
+    }
 </style>
