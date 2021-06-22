0e9d0644ad21398026731e3d7c69a9c3ddecd504
Bryant-Wang24 <485969746@qq.com>
2021-06-22 16:20:37 +0800

    完成三个基本页面，使用低版本的sass和sass-loader已解决报错

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    // component: ()=>import('../views/Money.vue'),
+    redirect:'/money'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/money',
+    component: ()=>import('../views/Money.vue'),
+  },
+  {
+    path: '/labels',
+    component: ()=>import('../views/Labels.vue')
+  },
+  {
+    path: '/statistics',
+    component: ()=>import('../views/Statistics.vue')
   }
 ]
 
